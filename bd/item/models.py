31c350765c633279b1b7a0e4e5279d1659a9a3b7
AG-h <129055916+AG-h@users.noreply.github.com>
2024-04-17 18:38:27 +0300

    Delete bd directory

--- bd/item/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Categoty(models.Model):
-    name = models.CharField(max_length=50)
-
-class Item(models.Model):
-     name = models.CharField(max_length=50)
-     category = models.CharField(max_length=50)
--- bd/item/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from item import views
-
-
-urlpatterns = [
-    path('', views.search, name='search')
-]
